b35ffd040cc315cc264029494310958845c7f0e4
jihunjjangjjangman <kamdong777777@gmail.com>
2022-12-20 11:51:13 +0900

    estimate Correct, Correct count funciont

--- C_gittest/C_gittest/main.cpp
+++ C_gittest/C_gittest/main.cpp
@@ -1,21 +1,36 @@
+#pragma warning(disable:4996)
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 #include<iostream>
+#define _CRT_SECURE_NO_WARNINGS
 using namespace std;
-int num1, num2, result, pm, ans;
+int num1, num2, result, pm, ans, correct_cnt;
 char artimatic;
+void random();
 int main() {
+	correct_cnt = 1;
 	srand(time(NULL));
-	random();
-	printf("%d %c %d = ", num1, artimatic, num2);
-	scanf("%d", &ans);
+	while (correct_cnt<10) {
+
+		random();
+		printf("[%d/10] %d %c %d = ",correct_cnt, num1, artimatic, num2);
+		scanf("%d", &ans);
+		if (ans == result) {
+			printf("Correct!\n");
+			correct_cnt++;
+		}
+		else {
+			printf("Wrong! Answer is %d\n", result);
+		}
+
+	}
 }
 
 void random() {
-	int num1 = rand() % 20 + 1;
-	int num2 = rand() % 20 + 1;
-	int pm = rand() % 4 + 1;
+	num1 = rand() % 20 + 1;
+	num2 = rand() % 20 + 1;
+	pm = rand() % 4 + 1;
 	switch (pm) {
 	case 1:
 		result = num1 + num2;
